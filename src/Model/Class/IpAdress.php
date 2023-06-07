ffa94ae1317ccd8886ae96c091f3be50dcf50c33
wajdi ben helal <114242095+BenHelal@users.noreply.github.com>
2023-06-07 10:01:01 -0300

    done

--- src/Model/Class/IpAdress.php
+++ src/Model/Class/IpAdress.php
@@ -8,7 +8,7 @@ class IpAdress
     public function getIpAdress()
     {
         $branch = trim(shell_exec('git branch --show-current'));
-        if($branch == 'local' | $branch == 'local2'){
+        if($branch == 'local' | $branch == 'local2'| $branch == 'local4'){
             return "10.100.1.245";
         }elseif($branch == 'prod'| $branch == 'prod1'){
             return "sistemas.serdia.com.br";
@@ -16,7 +16,8 @@ class IpAdress
         }elseif($branch == 'test' | $branch == 'test2'  | $branch == 'test4' | $branch == 'test5'){
             return "10.100.1.180";
         }else{
-            return "sistemas.serdia.com.br";
+            //return "sistemas.serdia.com.br";    
+            return "10.100.1.245";
         }
     }
 }
