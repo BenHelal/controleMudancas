07c0e8221ba7d15dc8ab9c7a162b5330e2fd7d44
wajdi ben helal <114242095+BenHelal@users.noreply.github.com>
2023-06-16 10:59:24 -0300

    fix ip adress

--- src/Model/Class/IpAdress.php
+++ src/Model/Class/IpAdress.php
@@ -10,15 +10,15 @@ class IpAdress
         $branch = trim(shell_exec('git branch --show-current'));
         if($branch == 'local' | $branch == 'local2'| $branch == 'local4'){
             return "10.100.1.245";
-        }elseif($branch == 'prod'| $branch == 'prod1'){
+        }elseif($branch == 'prod'| $branch == 'prod1' | $branch == 'prod2'){
             return "sistemas.serdia.com.br";
 
         }elseif($branch == 'test' | $branch == 'test2'  | $branch == 'test4' | $branch == 'test5'| $branch == 'test6'){
             return "10.100.1.180";
         }else{
-            //return "sistemas.serdia.com.br";    
+            return "sistemas.serdia.com.br";    
             //return "10.100.1.245";
-            return "10.100.1.180";
+            //return "10.100.1.180";
         }
     }
 }
